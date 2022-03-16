fce3faf528b87dc7f6ef37ad94fd61d2af5a7738
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2022-03-16 12:42:07 +0300

    add project options

---
+++ project/settings.py
@@ -0,0 +1,56 @@
+import os
+
+from dotenv import load_dotenv
+from environs import Env
+
+env = Env()
+env.read_env()
+
+load_dotenv()
+
+db_engine = os.getenv('ENGINE')
+db_host = os.getenv('HOST')
+port = os.getenv('PORT')
+user_name = os.getenv('NAME')
+user_type = os.getenv('USER')
+user_password = os.getenv('PASSWORD')
+
+DATABASES = {
+    'default': {
+        'ENGINE': db_engine,
+        'HOST': db_host,
+        'PORT': port,
+        'NAME': user_name,
+        'USER': user_type,
+        'PASSWORD': user_password,
+    }
+}
+
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = 'REPLACE_ME'
+
+DEBUG = env.bool('DEBUG')
+
+ROOT_URLCONF = 'project.urls'
+
+ALLOWED_HOSTS = ['*']
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+    },
+]
+
+USE_L10N = True
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
